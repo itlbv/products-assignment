28168845361d348bcde93414500ec19847cc12db
a.fokin <a.fokin@sportradar.com>
2021-05-10 10:33:06 +0200

    Add @Id to Product and Variant

--- src/main/java/no/ordr/products/domain/Product.java
+++ src/main/java/no/ordr/products/domain/Product.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
@@ -13,7 +14,9 @@ import org.springframework.data.elasticsearch.annotations.Document;
 @Document(indexName = "products")
 public class Product {
 
+  @Id
   private String id;
+
   private String name;
   private String description;
   private int version;
--- src/main/java/no/ordr/products/domain/Variant.java
+++ src/main/java/no/ordr/products/domain/Variant.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class Variant {
 
+  @Id
   private String id;
+
   private String variantName;
   private long quantity;
   private String unit;
